8548141f4914a18495e36a9a70ace48d32560959
Unknown <pedro.venturott@gmail.com>
2017-04-29 17:39:45 -0300

    Primeira implementação do código completo

--- Triciclo.c
+++ Triciclo.c
@@ -8,13 +8,11 @@
 
 #define USE_AHB
 
-uint16_t test_variable = 0;
-
-
-
-
-
 #include "Triciclo.h"
+
+uint16_t test_variable = 0;
+uint8_t flag_motor = OFF;
+uint8_t flag_voltage_level = LOW;
 
 void IoInit(void)   //pin initialization
 {
@@ -23,8 +21,27 @@ void IoInit(void)   //pin initialization
     GPIOInit(GREEN_LED, OUTPUT);
     GPIOInit(CHARGE_MODE_PIN, INPUT);
     GPIOInit(DRIVE_MODE_PIN, INPUT);
+    GPIOInit(START_MOTOR_PIN, INPUT);
     GPIOInit(CHARGE_PIN,OUTPUT);
     GPIOInit(MOTOR_PIN,OUTPUT);
+    TurnOffMotor;
+    TurnOffCharge;
+}
+
+void TimersInit(void)
+{
+    ConfigureTimer(TIMER_0, PERIODIC_MODE, TIMER_INTERRUPT_ENABLE, ONE_SEC);    //timer for current read
+    ConfigureTimer(TIMER_1, PERIODIC_MODE, TIMER_INTERRUPT_ENABLE, ONE_SEC);    //timer for voltage read
+    ConfigureTimer(TIMER_2, ONE_SHOT_MODE, TIMER_INTERRUPT_ENABLE, FIVE_SEC);   //timer for stopping motor
+}
+
+void ADCInit(void)
+{
+    ConfigureADC(CURRENT_READ_PIN, ADC_0, AIN8, SS_0, PROCESSOR, ADC_INTERRUPT_ENABLE); //configure ADC read for current
+    ConfigureADC(VOLTAGE_READ_PIN, ADC_0, AIN9, SS_1, PROCESSOR, ADC_INTERRUPT_ENABLE); //configure ADC read for voltage
+    EnableSampleSequencer(ADC_0, SS_0); //enables ADC read for current
+    EnableSampleSequencer(ADC_0, SS_1); //enables ADC read for voltage
+
 }
 
 void IntInit(void)  //initialize interrupts
@@ -46,68 +63,108 @@ void TIMER16_0A_IRQHandler(void)
     StartCurrentRead;
 }
 
+void TIMER16_1A_IRQHandler(void)
+{
+    
+    ClearTimerInterruptStatus(TIMER_1);
+    StartVoltageRead;
+}
+
+void TIMER16_2A_IRQHandler(void)
+{
+    
+    ClearTimerInterruptStatus(TIMER_2);
+    TurnOffMotor;
+
+}
+
 void ADC0Seq0_IRQHandler(void)
 {
     ClearADCInterruptStatus (ADC_0,SS_0);    //clear the interrupt status so program can continue
-    
 
-    test_variable = !test_variable;
-
-
-    if(test_variable == 1)
+    if(CurrentRead <= CURRENT_BATTERY_FULL)
     {
-        TurnOnMotor;
+        TurnOffCharge;
         TurnOnWhiteLed;
-    } 
+    }
     else
     {
-        TurnOffMotor;
-        TurnOnRedLed;
+        TurnOnCharge;
+        TurnOnBlueLed;
     }
-   // while(1){}
-    
+}
 
+void ADC0Seq1_IRQHandler(void)
+{
+    ClearADCInterruptStatus (ADC_0,SS_1);    //clear the interrupt status so program can continue
+    
+    if(DriveModePin == ON)  //voltage read in drive mode, for checking if battery has enough power
+    {
+        if(VoltageRead > VOLTAGE_BATTERY_EMPTY)
+        {
+            flag_voltage_level = OK;
+            TurnOnGreenLed;
+        } 
+        else
+        {
+            flag_voltage_level = LOW;
+            TurnOnRedLed;
+        }
+    }
+    else    //voltage read in charge mode, to check if battery isn't full
+    {
+        if(VoltageRead < VOLTAGE_BATTERY_FULL)
+        {
+            TurnOnBlueLed;
+            TurnOnCharge;
+            EnablePeriodicCurrentRead;
+        }
+        else
+        {
+            TurnOnWhiteLed;
+        }
+    }
+    
 }
 
 int main(void) {	//função main **** Lembrar de inicializar portas ****
 
 IoInit();        //port initialization
+TimersInit();   //timers initialization
+ADCInit();      //ADC modules initialization
 IntInit();      //interrupt initialization
 
-ConfigureADC(VOLTAGE_READ_PIN, ADC_0, AIN9, SS_0, PROCESSOR, ADC_INTERRUPT_ENABLE);
-EnableSampleSequencer (ADC_0, SS_0);
 
-ConfigureTimer(TIMER_0, PERIODIC_MODE, TIMER_INTERRUPT_ENABLE, FIVE_SEC);   //
-
-EnableTimer(TIMER_0);
-
-while(1){
+while(1){   //main loop
 
     if(ChargeModePin == ON)   //charge mode
     {
-        /* faz um monte de coisa
+        StartVoltageRead;
 
+        while(ChargeModePin == ON){}    
 
-        
-        */
-        TurnOnBlueLed;
-        while(ChargeModePin == ON){}
+        DisablePeriodicCurrentRead;
+        TurnOffCharge; 
     }   //end of charge mode
 
     
     if(DriveModePin == ON)     //drive mode
     {
-        TurnOnWhiteLed;
-        
+        EnablePeriodicVoltageRead;
 
         while(DriveModePin == ON)
-        {     
-
-        }   
+        {
+            if((StartMotorPin == ON) && (flag_voltage_level == OK))
+            {
+                TurnOnMotor;
+            }
+            while(flag_motor == ON){}
+        }
     }   //end of drive mode
+
 }   //end of primary loop
 
 return 0;
-}
+}   //end of main
 
 
--- Triciclo.h
+++ Triciclo.h
@@ -12,10 +12,15 @@ extern "C" {
 #include	"ADC.h"
 #include	"Timers.h"
 
+extern uint16_t test_variable;
+extern uint8_t flag_motor;
+extern uint8_t flag_voltage_level;
+
 const i_o CHARGE_MODE_PIN =  {.port = PORT_A, .pin = 7};    //input pin for 'charge mode'
 const i_o DRIVE_MODE_PIN  =  {.port = PORT_A, .pin = 6};   //input pin for 'charge mode'
-const i_o CHARGE_PIN 	  =  {.port = PORT_B, .pin = 1};	//output pin for enabling charge
-const i_o MOTOR_PIN		  =	 {.port = PORT_B, .pin = 2};	//output pin for motor activation
+const i_o START_MOTOR_PIN =  {.port = PORT_A, .pin = 5};	//input pin for starting the motor
+const i_o CHARGE_PIN 	  =  {.port = PORT_B, .pin = 0};	//output pin for enabling charge
+const i_o MOTOR_PIN		  =	 {.port = PORT_B, .pin = 1};	//output pin for motor activation
 const i_o RED_LED         =  {.port = PORT_F, .pin = 1};    //red led
 const i_o BLUE_LED        =  {.port = PORT_F, .pin = 2};    //blue led
 const i_o GREEN_LED       =  {.port = PORT_F, .pin = 3};    //greeb led 
@@ -23,12 +28,21 @@ const i_o GREEN_LED       =  {.port = PORT_F, .pin = 3};    //greeb led
 const i_o VOLTAGE_READ_PIN = {.port = PORT_E, .pin = 4};	//Analog input for voltage read (ADC pin PE4)
 const i_o CURRENT_READ_PIN = {.port = PORT_E, .pin = 5};	//Analog input for current read (ADC pin PE5)
 
-#define		ONE_SEC			0x05F5E100	//time for periodic current and voltage reads
-#define		FIVE_SEC		0x1DCD6500	//motor activation time
+#define	OK	1		//flags for voltage level
+#define LOW 0
+
+#define	ONE_SEC			0x05F5E100	//time for periodic current and voltage reads
+#define	FIVE_SEC		0x1DCD6500	//motor activation time
+
+#define	VOLTAGE_BATTERY_EMPTY	0x02AA	//ADC read for 2.2 volts at ADC pin, equals 11 volts at battery
+#define VOLTAGE_BATTERY_FULL	0x0326	//ADC read for 2.6 volts at ADC pin, equals 13 volts at battery
+
+#define	CURRENT_BATTERY_FULL 	0x000F	//generic ADC read for low current
 
 
 #define	ChargeModePin   GPIOReadPin(CHARGE_MODE_PIN)			//reading of 'charging mode' pin
 #define	DriveModePin    GPIOReadPin(DRIVE_MODE_PIN)			//reading of "drive mode" pin
+#define	StartMotorPin	GPIOReadPin(START_MOTOR_PIN)
 
 #define TurnOffRedLed   GPIOWritePin(RED_LED, OFF)	//macros for turning led's off
 #define TurnOffBlueLed  GPIOWritePin(BLUE_LED, OFF)
@@ -41,16 +55,22 @@ const i_o CURRENT_READ_PIN = {.port = PORT_E, .pin = 5};	//Analog input for curr
 #define TurnOnWhiteLed  GPIOWritePin(RED_LED, ON); GPIOWritePin(BLUE_LED, ON); GPIOWritePin(GREEN_LED, ON)
 
 #define TurnOnCharge    GPIOWritePin(CHARGE_PIN,ON)
-#define TurnOnMotor     GPIOWritePin(MOTOR_PIN,ON)
+#define TurnOnMotor     GPIOWritePin(MOTOR_PIN,ON); EnableTimer(TIMER_2); flag_motor = ON
 
 #define TurnOffCharge   GPIOWritePin(CHARGE_PIN,OFF)
-#define TurnOffMotor    GPIOWritePin(MOTOR_PIN,OFF)
+#define TurnOffMotor    GPIOWritePin(MOTOR_PIN,OFF); flag_motor = OFF
 
 #define	StartCurrentRead	StartADCConversion(ADC_0, SS_0)
 #define	StartVoltageRead	StartADCConversion(ADC_0, SS_1)
 
-#define	GetCurrentRead	GetADCConversion(ADC_0, SS_0)	
-#define	GetVoltageRead	GetADCConversion(ADC_1, SS_0)
+#define	CurrentRead	GetADCConversion(ADC_0, SS_0)	
+#define	VoltageRead	GetADCConversion(ADC_0, SS_1)
+
+#define EnablePeriodicCurrentRead 		EnableTimer(TIMER_0)
+#define	DisablePeriodicCurrentRead		DisableTimer(TIMER_0)
+
+#define EnablePeriodicVoltageRead 		EnableTimer(TIMER_1)
+#define	DisablePeriodicVoltageRead		DisableTimer(TIMER_1)
 
 void IoInit(void);   //pin initialization
 void IntInit(void);  //initialize interrupts
